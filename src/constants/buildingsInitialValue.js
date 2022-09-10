c206b169f2e0ddc006057a908fb82cf93d13e1ba
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-10 13:48:14 -0300

    added initial states constants

---
+++ src/constants/buildingsInitialValue.js
@@ -0,0 +1,6 @@
+const BUILDINGS_INITIAL_VALUE = {
+  stays: null,
+  error: null,
+};
+
+export default BUILDINGS_INITIAL_VALUE;
---
+++ src/constants/stayFilterInitialValue.js
@@ -0,0 +1,7 @@
+const STAY_FILTER_INITIAL_VALUE = {
+  city: null,
+  guests: 0,
+  page: 1,
+};
+
+export default STAY_FILTER_INITIAL_VALUE;
