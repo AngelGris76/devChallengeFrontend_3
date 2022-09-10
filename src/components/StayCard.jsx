04b2b600f054986ed9bf061c92014188ebadb6f1
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-10 13:46:19 -0300

    added StayCard component

---
+++ src/components/StayCard.jsx
@@ -0,0 +1,26 @@
+import StarIcon from './icons/StarIcon';
+import style from './StayCard.module.css';
+
+const StayCard = ({ superHost, title, type, beds, rating, photo }) => {
+  return (
+    <article className={style.stayCard}>
+      <h2>{title}</h2>
+      <p className={style.stayDetail}>
+        {superHost && <span className={style.superHost}>super host</span>}
+        <span>{type}.</span>
+        {superHost && beds && <span>beds {beds}</span>}
+        <span className={style.rating}>
+          <span className={style.ratingIcon}>
+            <StarIcon width='1rem' />
+          </span>
+          {rating}
+        </span>
+      </p>
+      <div className={style.stayCardPhotoContainer}>
+        <img src={photo} alt='' className={style.stayCardPhoto} />
+      </div>
+    </article>
+  );
+};
+
+export default StayCard;
