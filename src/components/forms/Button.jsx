bf5224363c711d8275bab870255b6508bcaa06fb
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-10 13:44:03 -0300

    modified Button component

--- src/components/forms/Button.jsx
+++ src/components/forms/Button.jsx
@@ -7,7 +7,15 @@ const EXTRA_CLASS = {
   onlyIcon: style.onlyIcon,
 };
 
+const SIZE_CLASS = {
+  sm: style.smallButton,
+  md: style.mediumButton,
+};
+
 const Button = ({
+  size,
+  borderLess,
+  kind = 'button',
   text,
   icon: Icon,
   onlyIcon,
@@ -16,9 +24,15 @@ const Button = ({
   ...props
 }) => {
   const colorClass = EXTRA_CLASS[color || 'empty'];
+  const sizeClass = size ? SIZE_CLASS[size] : '';
+  const extraClass = borderLess ? style.borderLess : '';
 
   return (
-    <button className={`${style.button} ${colorClass}`} {...props}>
+    <button
+      className={`${style.button} ${colorClass} ${extraClass} ${sizeClass}`}
+      type={kind}
+      {...props}
+    >
       {Icon && <Icon width={iconWidth} />}
       {!onlyIcon && text}
     </button>
