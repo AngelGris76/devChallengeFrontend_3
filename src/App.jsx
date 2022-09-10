e585f35383e274ad7e80a155770341d8a056ccee
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-10 14:45:38 -0300

    refacto App with hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
-import { useEffect, useState } from 'react';
+import useStay from './hooks/useStay';
 import Header from './components/Header';
 import StayCard from './components/StayCard';
 import StayPagination from './components/StayPagination';
-import BUILDINGS_INITIAL_VALUE from './constants/buildingsInitialValue';
-import STAY_FILTER_INITIAL_VALUE from './constants/stayFilterInitialValue';
 
 import style from './App.module.css';
 
-const URL = '/assets/stays.json';
-
-const fetchingData = async (setBuildings) => {
-  try {
-    const response = await fetch(URL);
-    if (!response.ok) throw new Error('Server not respond');
-    const data = await response.json();
-    setBuildings({ stays: data, error: null });
-  } catch (err) {
-    setBuildings({ error: 'Server not respond' });
-  }
-};
-
 const App = () => {
-  const [buildings, setBuildings] = useState(BUILDINGS_INITIAL_VALUE);
-  const [stayFilter, setStayFilter] = useState(STAY_FILTER_INITIAL_VALUE);
-
-  const setCity = (newCity) => {
-    const newFilter = { ...stayFilter, city: newCity };
-    setStayFilter(newFilter);
-  };
-
-  const setGuests = (newGuests) => {
-    const newFilter = { ...stayFilter, guests: newGuests };
-    setStayFilter(newFilter);
-  };
-
-  const setPage = (newPage) => {
-    const newFilter = { ...stayFilter, page: newPage };
-    setStayFilter(newFilter);
-  };
-
-  useEffect(() => {
-    fetchingData(setBuildings);
-  }, []);
-
-  let filteredBuildings = filterByCity(buildings.stays, stayFilter.city);
-
-  filteredBuildings = filterByGuests(filteredBuildings, stayFilter.guests);
-
-  const totalPages = Math.ceil(filteredBuildings.length / 6);
-  const paginatedStays = paginateStay(filteredBuildings, stayFilter.page);
+  const { stayFilter, totalPages, totalStays, paginatedStays, setPage } =
+    useStay();
 
   const renderedStays = paginatedStays.map((stay, index) => (
     <StayCard key={index} {...stay} />
@@ -60,10 +19,8 @@ const App = () => {
       <main className={style.mainContainer}>
         <div className={style.title}>
           <h2>{`Stays in Finland`}</h2>
-          {buildings.stays && (
-            <span>{`${
-              filteredBuildings.length > 12 ? '12+' : filteredBuildings.length
-            } stays`}</span>
+          {totalStays && (
+            <span>{`${totalStays > 12 ? '12+' : totalStays} stays`}</span>
           )}
         </div>
         <div className={style.staysCardContainer}>{renderedStays}</div>
@@ -78,21 +35,3 @@ const App = () => {
 };
 
 export default App;
-
-const filterByCity = (buildings, cityFilter) => {
-  if (!buildings) return [];
-  if (!cityFilter) return [...buildings];
-  return buildings.filter(({ city }) => city === cityFilter);
-};
-
-const filterByGuests = (buildings, guestsFilter) => {
-  if (!buildings.length) return [...buildings];
-  if (!guestsFilter) return [...buildings];
-  return buildings.filter(({ maxGuests }) => maxGuests >= guestsFilter);
-};
-
-const paginateStay = (buildings, page) => {
-  const initialIndex = page * 6 - 6;
-  const finalIndex = initialIndex + 6;
-  return buildings.slice(initialIndex, finalIndex);
-};
