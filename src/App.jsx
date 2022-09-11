c7c387229ffee43e39eb0daeecd50502d60ee4fc
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-11 12:26:56 -0300

    App modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
+import { useState } from 'react';
 import useStay from './hooks/useStay';
 import Header from './components/Header';
 import StayCard from './components/StayCard';
 import StayPagination from './components/StayPagination';
+import FilterForm from './components/FilterForm';
 
 import style from './App.module.css';
 
 const App = () => {
-  const { stayFilter, totalPages, totalStays, paginatedStays, setPage } =
-    useStay();
+  const [showForm, setShowForm] = useState(false);
+  const {
+    stayFilter,
+    totalPages,
+    totalStays,
+    paginatedStays,
+    setPage,
+    setCity,
+    setGuests,
+  } = useStay();
 
   const renderedStays = paginatedStays.map((stay, index) => (
     <StayCard key={index} {...stay} />
   ));
 
+  const noScrollClass = showForm ? style.noScroll : '';
+
   return (
-    <>
-      <Header />
+    <div className={noScrollClass}>
+      {showForm && (
+        <FilterForm
+          showForm={showForm}
+          setShowForm={setShowForm}
+          setCity={setCity}
+          setGuests={setGuests}
+        />
+      )}
+      <Header stayFilter={stayFilter} setShowForm={setShowForm} />
       <main className={style.mainContainer}>
         <div className={style.title}>
           <h2>{`Stays in Finland`}</h2>
@@ -30,10 +50,11 @@ const App = () => {
           setPage={setPage}
         />
       </main>
+
       <footer className={style.footer}>
         <p>Created by Sebastian Smuraglia</p>
       </footer>
-    </>
+    </div>
   );
 };
 
