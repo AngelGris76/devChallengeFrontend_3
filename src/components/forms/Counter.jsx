8a08a37b506ef33c6bdeab92d89aa3d1a7df1ca4
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-11 12:21:58 -0300

    added compoents for number input

---
+++ src/components/forms/Counter.jsx
@@ -0,0 +1,34 @@
+import MinusIcon from '../icons/MinusIcon';
+import PlusIcon from '../icons/PlusIcons';
+import Button from './Button';
+
+import style from './Counter.module.css';
+
+const Counter = ({ item, description, value, setter }) => {
+  return (
+    <div className={style.counterContainer}>
+      <p className={style.counterItem}>{item}</p>
+      <p className={style.counterDescription}>{description}</p>
+      <div className={style.buttons}>
+        <Button
+          icon={MinusIcon}
+          onlyIcon
+          size='sm'
+          onClick={() => {
+            if (!value) return;
+            setter(value - 1);
+          }}
+        />
+        <span>{value}</span>
+        <Button
+          icon={PlusIcon}
+          onlyIcon
+          size='sm'
+          onClick={() => setter(value + 1)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
