f57783c18c9c77b995395e521a73b3d3600b954d
Sebastian Smuraglia <sebastian.smuraglia@gmail.com>
2022-09-11 12:23:36 -0300

    header modifications

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,36 @@
 import Button from './forms/Button';
 import LogoIcon from './icons/LogoIcon';
 import SearchIcon from './icons/SearchIcon';
+import SECTION_VALUES from '../constants/sectionValues';
 
 import style from './Header.module.css';
 
-const Header = () => {
+const Header = ({ stayFilter, setShowForm }) => {
+  const buttonCityText = stayFilter.city
+    ? `${stayFilter.city}, Finland`
+    : 'location';
+
+  const buttonGuestsText = stayFilter.guests
+    ? `${stayFilter.guests} guests`
+    : 'Add guests';
   return (
     <>
       <header className={style.header}>
         <LogoIcon />
         <div className={style.buttonsContainer}>
-          <Button text='location' color='filled' borderLess />
+          <Button
+            text={buttonCityText}
+            color={stayFilter.city ? 'filled' : 'empty'}
+            borderLess
+            onClick={() => setShowForm(SECTION_VALUES.city)}
+          />
           <span className={style.separator}></span>
-          <Button text='Add guests' color='empty' borderLess />
+          <Button
+            text={buttonGuestsText}
+            color={stayFilter.guests ? 'filled' : 'empty'}
+            borderLess
+            onClick={() => setShowForm(SECTION_VALUES.guests)}
+          />
           <span className={style.separator}></span>
           <Button
             icon={SearchIcon}
@@ -20,6 +38,7 @@ const Header = () => {
             onlyIcon
             color='onlyIcon'
             borderLess
+            onClick={() => setShowForm(SECTION_VALUES.city)}
           />
         </div>
       </header>
